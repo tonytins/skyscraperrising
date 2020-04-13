80140c7e00004b2eab1dacc5aecbec8ec895c22f
Anthony Foxclaw <35226681+tonytins@users.noreply.github.com>
2020-04-13 03:19:32 -0400

    Moved name and game status to the left

--- project/src/gpanel.gd
+++ project/src/gpanel.gd
@@ -1,11 +1,7 @@
 extends Panel
 
 func _process(delta):
-	$GameStatus/MoneyLbl.text = str(GameData.budget)
-	$GameStatus/YearNumLbl.text = str(GameData.year)
-	$GameStatus/PopNumLbl.text = str(GameData.population)
-	$GameStatus/QuaterNumLbl.text = str(GameData.quarter)
-	$GameMenus/NameBtn.text = GameData.tower_name
-
-func _on_NameBtn_pressed():
-	pass # Replace with function body.
+	$GameStats/MoneyLbl.text = str(GameData.budget)
+	$GameStats/YearNumLbl.text = str(GameData.year)
+	$GameStats/QuaterNumLbl.text = str(GameData.quarter)
+	$GameStats/NameLbl.text = GameData.tower_name
